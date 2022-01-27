5d75147c781776cec4ece4568d1e7cad135984dd
Noy Morgenshtein <noy9630@gmail.com>
2022-01-27 22:52:47 +0200

    *. Socket cursors

--- services/socket.service.js
+++ services/socket.service.js
@@ -9,14 +9,25 @@ let gIo = null;
 //   The value, will be the provided wap, so any user connected to the room will get the present-editing wap.
 
 // example : 
-// let gIo = {
+// let gIoooo = {
 //     sharedRooms: { 
 //         '35k1jasf': {
 //             wap: {},
-//             connectedUsers: 1
+//             connectedUsers: 1,
+//             cursors: ['socket.id_1']
 //         }
 //     }
 // }
+
+let gIoooooo = {
+    sharedRooms: { 
+        '35k1jasf': {
+            wap: {},
+            connectedUsers: 2,
+            cursors: ['socket.id_1','socket.id_2']
+        }
+    }
+}
 
 
 function connectSockets(http, session) {
@@ -27,81 +38,169 @@ function connectSockets(http, session) {
         }
     })
 
-    // gIo refers to ALL of our sockets
     gIo.on('connection', socket => {
         console.log('user connected', socket.id);
 
         socket.on('disconnect', socket => {
             console.log('user disconnected');
 
-            // If the user was connected to a room, update the connectedUsers count.
             if (socket.wapId) gIo.sharedRooms[socket.wapId].connectedUsers--;
-            // If the room has 0 connected users left, remove it.
             if (socket.wapId) {
                 if (!gIo?.sharedRooms[socket.wapId]?.connectedUsers) delete gIo.sharedRooms[socket.wapId];
             }
         })
 
-        // User clicked on "Work Together" link :
-        socket.on('create-room', wap => {
-
-            // Create  sharedRooms if it didn't exist.
-            if (!gIo.sharedRooms) gIo.sharedRooms = {};
-
-            // If the user is already in that room (has a wapId "sticker") return, no need to reconnect.
-            if (socket.wapId === wap.id) return;
-
-            // Leave whatever room you were in
-            if (socket.wapId) {
-                socket.leave(socket.wapId);
-            }
-
-            // Join the room provided with the wap.id
-            // If the room didnt exist, create it
-            if (!gIo.sharedRooms[wap.id]) gIo.sharedRooms[wap.id] = { wap, connectedUsers: 1 };
-
-            // "Stick" a label on the socket (a.k.a connect it to a room)
-            socket.join(wap.id);
-            // Save the joined room on the socket for our convinience
-            socket.wapId = wap.id;
-
+        socket.on('force-disconnect', () => {
+            socket.disconnect();
         })
 
-        // User opened the link he got from the other user :
-        socket.on('join-room', wapId => {
+        socket.on('create-room', wap => {
 
-            // If the user is already in that room (has a wapId "sticker") return, no need to reconnect
-            if (socket.wapId === wapId) return;
-            
-            // Leave whatever room you were in
+            if (!gIo.sharedRooms) gIo.sharedRooms = {};
+
+            if (socket.wapId === wap.id) return;
+
             if (socket.wapId) {
                 socket.leave(socket.wapId);
             }
-            
-            // "Stick" a label on the socket (a.k.a connect it to a room)
-            socket.join(wapId);
-            // Save the joined room on the socket for our convinience
-            socket.wapId = wapId;
-            
-            gIo.sharedRooms[wapId].connectedUsers++;
 
+            if (!gIo.sharedRooms[wap.id]) gIo.sharedRooms[wap.id] = { wap, connectedUsers: 1, cursors: [socket.id] };
+
+            socket.join(wap.id);
+            socket.wapId = wap.id;
+        })
+
+        socket.on('join-room', wapId => {
+
+            if (socket.wapId === wapId) return;
+
+            if (socket.wapId) {
+                socket.leave(socket.wapId);
+            }
+
+            socket.join(wapId);
+            socket.wapId = wapId;
+
+            gIo.sharedRooms[wapId].connectedUsers++;
+            gIo.sharedRooms[wapId].cursors.push(socket.id); // each cursor is a string of the socket's id
+            
             socket.emit('load-wap', gIo.sharedRooms[wapId].wap);
         })
 
-
         socket.on('update-wap', wap => {
 
-            // Update the room's wap :
             gIo.sharedRooms[wap.id].wap = wap;
-            // Update to all users except the sender :
             socket.to(wap.id).emit('wap-updated', wap);
         })
 
+        socket.on('mouse-move', pos => {
+            if (!socket.wapId) return;
+            socket.to(socket.wapId).emit('mouse-moved', {id: socket.id, pos});
+        })
     })
-
 }
 
 
 module.exports = {
     connectSockets
 }
+
+
+
+
+
+
+
+
+
+
+
+// function connectSockets(http, session) {
+
+//     gIo = require('socket.io')(http, {
+//         cors: {
+//             origin: '*'
+//         }
+//     })
+
+//     // gIo refers to ALL of our sockets
+//     gIo.on('connection', socket => {
+//         console.log('user connected', socket.id);
+
+//         socket.on('disconnect', socket => {
+//             console.log('user disconnected');
+
+//             // If the user was connected to a room, update the connectedUsers count.
+//             if (socket.wapId) gIo.sharedRooms[socket.wapId].connectedUsers--;
+//             // If the room has 0 connected users left, remove it.
+//             if (socket.wapId) {
+//                 if (!gIo?.sharedRooms[socket.wapId]?.connectedUsers) delete gIo.sharedRooms[socket.wapId];
+//             }
+//         })
+
+//         // User clicked on "Work Together" link :
+//         socket.on('create-room', wap => {
+
+//             // Create  sharedRooms if it didn't exist.
+//             if (!gIo.sharedRooms) gIo.sharedRooms = {};
+
+//             // If the user is already in that room (has a wapId "sticker") return, no need to reconnect.
+//             if (socket.wapId === wap.id) return;
+
+//             // Leave whatever room you were in
+//             if (socket.wapId) {
+//                 socket.leave(socket.wapId);
+//             }
+
+//             // Join the room provided with the wap.id
+//             // If the room didnt exist, create it
+//             if (!gIo.sharedRooms[wap.id]) gIo.sharedRooms[wap.id] = { wap, connectedUsers: 1 };
+
+//             // "Stick" a label on the socket (a.k.a connect it to a room)
+//             socket.join(wap.id);
+//             // Save the joined room on the socket for our convinience
+//             socket.wapId = wap.id;
+
+//         })
+
+//         // User opened the link he got from the other user :
+//         socket.on('join-room', wapId => {
+
+//             // If the user is already in that room (has a wapId "sticker") return, no need to reconnect
+//             if (socket.wapId === wapId) return;
+
+//             // Leave whatever room you were in
+//             if (socket.wapId) {
+//                 socket.leave(socket.wapId);
+//             }
+
+//             // "Stick" a label on the socket (a.k.a connect it to a room)
+//             socket.join(wapId);
+//             // Save the joined room on the socket for our convinience
+//             socket.wapId = wapId;
+
+//             gIo.sharedRooms[wapId].connectedUsers++;
+            
+//             // Send the wap
+//             socket.emit('load-wap', gIo.sharedRooms[wapId].wap);
+
+//             socket.to(wapId).emit('add-cursor', socket.id);
+//         })
+
+
+//         socket.on('update-wap', wap => {
+
+//             // Update the room's wap :
+//             gIo.sharedRooms[wap.id].wap = wap;
+//             // Update to all users except the sender :
+//             socket.to(wap.id).emit('wap-updated', wap);
+//         })
+
+
+//         socket.on('mouse-move', pos => {
+//             if (!socket.wapId) return;
+//             socket.to(socket.wapId).emit('mouse-moved', pos);
+//         })
+        
+//     })
+// }
